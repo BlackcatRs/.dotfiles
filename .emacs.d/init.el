1f0491633c940b483c403648f4d624e3bc465a62
vts <vithurshan@laposte.net>
2022-04-18 18:07:20 +0200

    some refactoring

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -130,18 +130,3 @@
 (use-package ivy-rich
   :init
   (ivy-rich-mode))
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ivy-rich which-key rainbow-delimiters doom-modeline ivy command-log-mode use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
