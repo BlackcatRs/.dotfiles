8200403eb649e4398cfd8dd4c5e3f8432c0b0e20
vts <vithurshan@laposte.net>
2022-06-19 12:10:20 +0200

    change emacs theme to doom-one

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -119,7 +119,7 @@
 
 ;; Download doom themes
 (use-package doom-themes)
-(load-theme 'doom-dark+ t)
+(load-theme 'doom-one t)
 
 ;; Show parentheese
 (use-package rainbow-delimiters
@@ -179,6 +179,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
  '(package-selected-packages
    '(nasm-mode masm-mode helpful ivy-rich which-key rainbow-delimiters doom-themes doom-modeline counsel use-package)))
 (custom-set-faces
