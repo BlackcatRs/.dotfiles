6a19dbd61cf7c550b2fbc9485e8c5d60bee61308
vts <vithurshan@laposte.net>
2022-07-02 22:08:33 +0200

    save all emacs's backup file to .emacs.d/backup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -173,6 +173,14 @@
 ;; syntax.
 (use-package nasm-mode)
 
+;; Save all emacs backup files (files ending in ~) in ~/.emacs.d/backup
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+ )
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
