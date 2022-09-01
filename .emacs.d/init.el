6243b8f0029dbc662c0ded536f17baae8e686afb
vts <vithurshan@laposte.net>
2022-09-01 21:36:45 +0200

    Replace CapsLock with CTRL

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -119,7 +119,7 @@
 
 ;; Download doom themes
 (use-package doom-themes)
-(load-theme 'doom-dark+ t)
+(load-theme 'doom-one t)
 
 ;; Show parentheese
 (use-package rainbow-delimiters
@@ -233,9 +233,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
  '(package-selected-packages
-   '(markdown-mode general powershell nasm-mode masm-mode helpful ivy-rich which-key rainbow-delimiters doom-themes doom-modeline counsel use-package)))
+   '(markdown-mode powershell general nasm-mode masm-mode helpful ivy-rich which-key rainbow-delimiters doom-themes doom-modeline counsel use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
