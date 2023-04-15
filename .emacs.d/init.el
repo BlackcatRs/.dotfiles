808c320ab16f27a8699a31eb38f0b94b2f5ebf6c
vts <vithurshan@laposte.net>
2023-04-15 12:14:07 +0200

    Load Emacs config from README.org file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,11 @@
-(package-initialize)
+;; Method 1
+(require 'org)
 (org-babel-load-file "~/.dotfiles/.emacs.d/README.org")
+
+;; Method 2
+;(setq org-babel-load-languages '((emacs-lisp . t)))
+;(setq org-confirm-babel-evaluate nil)
+;(require 'org-install)
+;(require 'org)
+
+;(org-babel-load-file "~/.emacs.d/config.org")
