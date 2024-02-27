2dbb9a5f76d83eafb91ebe7c873cc85bac626809
vts <vithurshan@laposte.net>
2024-02-27 20:20:43 +0100

    i3 - shortcut to open fzfopen.sh script

---
+++ .local/bin/fzfopen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -eu
+
+# helpf() {
+#     echo 'Select and open a binary file using xdg-open(1).'
+#     echo 'The list of files is built using rg(1), and excludes all text file'
+#     echo 'types know to that command (see rg --type-list), which can be opened'
+#     echo 'in a terminal using nvim(1) or similar instead.'
+#     echo 'Requires the `fzfpreview.sh` script for (some) file previews.'
+# }
+
+# while getopts "h" OPT; do
+#     case "$OPT" in
+#         h ) helpf ; exit 0 ;;
+#         * ) helpf ; exit 1 ;;
+#     esac
+# done
+
+# rg --files --hidden --binary --type-clear=pdf --type-not=all --no-messages --no-ignore-vcs| \
+#     fzf --height 100% --bind "enter:execute(setsid xdg-open {} 2>/dev/null &)+abort" \
+#     --preview "fzfpreview.sh {}" --preview-window "down:60%:sharp"
+
+cd ~/
+fzf --bind "enter:execute(setsid xdg-open {} 2>/dev/null &)+abort"
