dd0a5e907cb6a60164f40f1c7d25ee6815a4f248
vts <vithurshan@laposte.net>
2025-08-28 12:06:09 +0200

    Emacs - auto added custom variables

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -9,3 +9,22 @@
 ;(require 'org)
 
 ;(org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(counsel-projectile dockerfile-mode doom-modeline doom-themes helpful
+			hideshowvis ivy-rich langtool magit
+			markdown-mode nasm-mode org-bullets org-mime
+			org-roam org-roam-ui org-wild-notifier ox-hugo
+			powershell rainbow-delimiters rainbow-mode
+			toc-org visual-fill-column web-mode which-key
+			yaml-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
